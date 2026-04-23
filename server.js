0ca625964f82002f155eebb5db26c996149e795a
angel-prz <191927151+angel-prz@users.noreply.github.com>
2026-04-22 22:02:17 -0300

    feat: add conexao com o banco

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 //App Config
 const app = express();
 const port = process.env.PORT || 9000;
+const connection_url = " mongodb//localhost:27017/chat";
 //Middleware
 //DB Config
+mongoose.connect(connection_url);
 //API Endpoints
 app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"));
 //Listener
